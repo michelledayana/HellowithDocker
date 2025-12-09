8d354a42aa9a3041a377068e7c24f01a1664018a
michelledayana <michurivadeneira98@gmail.com>
2025-12-09 15:47:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola Mundo desde Docker en el puerto 3002!");
+  res.send("Hello world, Distributed!!");
 });
 
 app.listen(3002, () => {
-  console.log("Servidor corriendo en el puerto 3002");
+  console.log("Server running on port 3002");
 });
